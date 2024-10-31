c80f7dd563b1358472b1a4411480cbc70212a01a
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2024-10-31 19:04:40 -0400

    Update voice_text.py

--- Basic_Voice_Assistant/voice_text.py
+++ Basic_Voice_Assistant/voice_text.py
@@ -1,22 +1,30 @@
 # voice_text.py
 
 import speech_recognition as sr
+import emotion_detection
+import matplotlib.pyplot as plt
+import librosa
+import numpy as np
+
 
 def convert_speech_to_text():
     # Initialize recognizer
     recognizer = sr.Recognizer()
     recognizer.pause_threshold = 0.8  # Stop listening after 2 seconds of silence
 
-
     # Capture audio from microphone
     with sr.Microphone() as source:
         print("Listening...")
         audio = recognizer.listen(source, phrase_time_limit=10)
+        # Ensure audio_np has the correct number of time steps (e.g., 162) for the model
+
+    emotion = emotion_detection.predict(audio)
 
     # Convert speech to text
     try:
         text = recognizer.recognize_google(audio)
         print("You said:", text)
+        print("With emotion: ", emotion)
         return text
     except sr.UnknownValueError:
         print("Sorry, I could not understand the audio.")
